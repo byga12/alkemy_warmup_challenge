c8fffcdc4f563492ee29c56722fee544c00ad91c
Gabriel Shimabuku <gaby.shima12@gmail.com>
2021-11-19 09:08:09 -0300

    HomePage now shows a spinner while fetching posts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,25 +2,34 @@ import { useEffect, useState } from "react"
 import Post from "../components/Post";
 import { fetchPosts } from "../services/api"
 
+//styles
+import s from './HomePage.module.css'
 
-
+//React-bootstrap
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function HomePage() {
 
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(()=> {
-    fetchPosts().then(res => setPosts(res.data))
+    setIsLoading(true);
+    fetchPosts().then(res => {
+      setPosts(res.data)
+      setIsLoading(false)
+    })
   }, [])
 
   return (
-    <div>
-    
-      {posts ?
-        posts.map(post => <Post key={post.id} {...post}/>) 
-        : null
+    <div className={s.container}>
+      
+      {isLoading ?
+        <Spinner animation="border" variant="info" style={{marginBlock:"2rem"}}/>
+        : posts ?
+            posts.map(post => <Post key={post.id} {...post}/>) 
+           : null
       }
-
 
     </div>
   )
